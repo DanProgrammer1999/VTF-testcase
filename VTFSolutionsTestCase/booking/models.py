18077d50de1ba42a18863c3c9268618129e76eb2
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2020-04-26 13:32:43 +0300

    better representation of models

--- VTFSolutionsTestCase/booking/models.py
+++ VTFSolutionsTestCase/booking/models.py
@@ -27,8 +27,14 @@ class Room(models.Model):
     name = models.CharField(max_length=100)
     room_category = models.ForeignKey(RoomCategory, related_name='rooms', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} ({})".format(self.name, self.room_category.name)
+
 
 class Booking(models.Model):
     room = models.ForeignKey(Room, related_name='room_bookings', on_delete=models.CASCADE)
     date_check_in = models.DateField(name='check in date')
     date_check_out = models.DateField(name='check out date')
+
+    def __str__(self):
+        return "{} ({}) from {} to {}".format(self.room.name, self.room.room_category.name, self.date_check_in, self.date_check_out)
